2804acb5ea498ac280595d45e578721cb8d97741
atanas011 <atanas011@yahoo.com>
2023-02-25 18:11:05 +0100

    Clipboard Test

---
+++ apps/selendroid.md
@@ -0,0 +1,2 @@
+Test apk downloaded from:  
+http://selendroid.io
---
+++ src/test/java/tests/ClipboardTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ClipboardTest extends BaseTest {
+
+    @Test
+    public void testClipboard() {
+        String text = "Hello";
+        driver.setClipboardText(text);
+        WebElement nameTxt = driver.findElement(
+                AppiumBy.accessibilityId("my_text_fieldCD"));
+        nameTxt.clear();
+        nameTxt.sendKeys(driver.getClipboardText());
+        Assert.assertEquals(text, nameTxt.getText());
+    }
+}
